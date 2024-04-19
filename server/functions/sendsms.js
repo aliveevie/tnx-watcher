711099f2efd5367ebd90cc136bd2a39644804997
aliveevie <aliveibi080@gmail.com>
2024-04-19 16:22:39 +0100

    Send messages via whatapp

--- server/functions/sendsms.js
+++ server/functions/sendsms.js
@@ -1,6 +1,9 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
+const dotenv = require('dotenv');
+dotenv.config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
@@ -8,7 +11,7 @@ const client = require('twilio')(accountSid, authToken);
 client.messages
   .create({
      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+15017122661',
-     to: '+15558675310'
+     from: 'whatsapp:+14155238886',
+     to: 'whatsapp:+23489036476869, whatsapp:+2348138300357',
    })
-  .then(message => console.log(message.sid));
+  .then(message => console.log(message.sid, message.status));
